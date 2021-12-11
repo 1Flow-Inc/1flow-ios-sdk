98fea4ad51eeea05b131f87bf5d035c7bfded9b4
rohantryskybox <rohan@tryskybox.com>
2021-12-11 16:43:54 +0530

    add support for SPM

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "1Flow",
+    platforms: [
+           .iOS(.v12),
+       ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "1Flow",
+            targets: ["1Flow"]),
+    ],
+   
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    
+    targets: [
+        .target(
+            name: "1Flow",
+            path: "Survey/Classes")
+    ]
+    
+)
