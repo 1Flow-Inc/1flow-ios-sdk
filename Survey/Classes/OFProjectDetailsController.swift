1dd7854ec74f3bc17935b7075e7e805548989c7e
Rohan Moradiya <rohan@tryskybox.com>
2023-06-20 01:00:21 +0530

    Release 2023.06.21

--- Survey/Classes/OFProjectDetailsController.swift
+++ Survey/Classes/OFProjectDetailsController.swift
@@ -61,7 +61,7 @@ protocol ProjectDetailsManageable {
 final class OFProjectDetailsController: NSObject, ProjectDetailsManageable {
 
     static let shared = OFProjectDetailsController()
-    let oneFlowSDKVersion: String = "2023.06.20"
+    let oneFlowSDKVersion: String = "2023.06.21"
     var currentEnviromment: OneFlowEnvironment = .prod
     var currentLogLevel: OneFlowLogLevel = .none
 
