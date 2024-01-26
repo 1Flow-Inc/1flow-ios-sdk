f5d9ac10a78a1a77a95a6a1ff03d8047e7c1de60
Rohan Moradiya <rohan@tryskybox.com>
2024-01-26 13:09:42 +0530

    Obj c fixes

--- Survey/Classes/Models/AnnouncementsDetails.swift
+++ Survey/Classes/Models/AnnouncementsDetails.swift
@@ -1,9 +1,16 @@
+// Copyright 2021 1Flow, Inc.
 //
-//  AnnouncementsDetails.swift
-//  1Flow
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Rohan Moradiya on 28/10/23.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import Foundation
 
