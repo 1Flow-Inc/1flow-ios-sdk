7da63bca059696f31c4011429d1afb1de628aa3e
rohantryskybox <79245762+rohantryskybox@users.noreply.github.com>
2024-05-15 11:28:10 +0530

    Updated pod version (#15)

--- Survey/Classes/OFProjectDetailsController.swift
+++ Survey/Classes/OFProjectDetailsController.swift
@@ -61,7 +61,7 @@ protocol ProjectDetailsManageable {
 final class OFProjectDetailsController: NSObject, ProjectDetailsManageable {
 
     static let shared = OFProjectDetailsController()
-    let oneFlowSDKVersion: String = "2024.01.26"
+    let oneFlowSDKVersion: String = "2024.05.15"
     var currentEnviromment: OneFlowEnvironment = .prod
     var currentLogLevel: OneFlowLogLevel = .none
 
